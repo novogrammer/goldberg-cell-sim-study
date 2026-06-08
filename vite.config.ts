14d8e8ce1f677e341c8044cff24b80f3bc145e97
Yusuke Kawamoto <novogrammer@gmail.com>
2026-06-08 17:44:02 +0900

    チャンク分割

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,23 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("/node_modules/three/")) {
+            return "three";
+          }
+
+          if (id.includes("/node_modules/")) {
+            return "vendor";
+          }
+
+          return undefined;
+        }
+      }
+    }
+  },
   test: {
     environment: "node",
     include: ["src/**/*.test.ts"]
